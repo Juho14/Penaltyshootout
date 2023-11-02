8e436eadd214f4475e16fbe7512cfacc734d476d
Juho14 <juqiqekli@gmail.com>
2023-11-02 23:20:10 +0200

    Penaltyshot target.
    Chooses the target location for a penalty shot.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Randomizer from './components/Randomizer';
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* Randomizer component */}
+      <Randomizer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Randomizer.jsx
@@ -0,0 +1,84 @@
+// Randomizer.js
+import React, { useState } from 'react';
+
+function Randomizer() {
+    const [scenario, setScenario] = useState(null);
+
+    const scenarios = [
+        {
+            description: "Top left",
+            targetX: 10,
+            targetY: 23,
+        },
+        {
+            description: "Bottom left",
+            targetX: 12,
+            targetY: 70,
+        },
+        {
+            description: "Top right",
+            targetX: 90,
+            targetY: 22,
+        },
+        {
+            description: "Bottom right",
+            targetX: 89,
+            targetY: 68,
+        },
+        {
+            description: "Middle",
+            targetX: 50,
+            targetY: 50,
+        },
+
+    ];
+
+    const handleRandomize = () => {
+        const randomIndex = Math.floor(Math.random() * scenarios.length);
+        setScenario(scenarios[randomIndex]);
+    };
+
+    return (
+        <div>
+            <div className="image-container">
+                <div className="goal-container">
+                    <img
+                        src="/football_goal.png"
+                        alt="Football Goal"
+                        style={{
+                            width: '600px',
+                            height: 'auto',
+                        }}
+                    />
+                    {scenario && (
+                        <img
+                            src="/football.png"
+                            alt="Football"
+                            className="football-image"
+                            style={{
+                                width: '30px',
+                                height: '30px',
+                                position: 'absolute',
+                                top: `${scenario.targetY}%`,
+                                left: `${scenario.targetX}%`,
+                                transform: 'translate(-50%, -50%)',
+                            }}
+                        />
+                    )}
+                </div>
+            </div>
+
+            {scenario ? (
+                <div>
+                    <p>{scenario.description}</p>
+                    <button onClick={handleRandomize}>Randomize Again</button>
+                </div>
+            ) : (
+                // Show a button to generate the first random scenario.
+                <button onClick={handleRandomize}>Randomize Scenario</button>
+            )}
+        </div>
+    );
+}
+
+export default Randomizer;
